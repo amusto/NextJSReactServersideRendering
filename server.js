bb940b093b34a270c590304761c81778cd6ff0fe
Armando Musto <armando.musto@gmail.com>
2019-01-14 18:17:13 -0500

    Commit of working example

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+console.log('Running dev?', dev)
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -35,9 +37,22 @@ app.prepare().then(() => {
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  // if (!dev) {
+  //   console.log('Running in Production')
+  //   // Express will serve up production assets
+  //   app.use(express.static('client/build'))
+  //
+  //   // Express will serve up the index.html file as the default if it can't find it in our route files above
+  //   const path = require('path')
+  //   app.get('*', (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  //   })
+  // }
+
+  const PORT = process.env.PORT || 3000
+  server.listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${PORT}`)
   })
 }).catch((ex) => {
   console.error(ex.stack)
