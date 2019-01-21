abac230615f977f37564b11730b0838b46c91c54
Armando Musto <armando.musto@gmail.com>
2019-01-21 13:23:26 -0500

    Troubleshooting build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
       config.module.rules.push({
         test: /\.js$/,
         exclude: /node_modules/,
-        // use: ['babel-loader', 'eslint-loader'],
         loader: 'eslint-loader',
         options: {
           // eslint options (if necessary)
