f4bfab25b919089edbbfc2aadb35c864d73aa5b9
Armando Musto <armando.musto@gmail.com>
2019-01-22 08:09:30 -0500

    Added Footer

---
+++ components/Footer.js
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import React from 'react'
+
+const footerStyle = {
+  marginTop: '200px'
+}
+
+const Footer = () => (
+  <div style={footerStyle}>
+    <Link href='https://github.com/amusto/RemedyHealthNextCodeTest'>
+      <a>https://github.com/amusto/RemedyHealthNextCodeTest</a>
+    </Link>
+    <style jsx>{`
+      input[type=text], select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+      }
+
+      input[type=submit] {
+        width: 100%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+
+      input[type=submit]:hover {
+        background-color: #45a049;
+      }
+
+      div {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 20px;
+      }
+
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        margin-right: 15px
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </div>
+)
+
+export default Footer
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 const layoutStyle = {
   margin: 20,
@@ -11,6 +12,7 @@ const Layout = (props) => (
   <div style={layoutStyle}>
     <Header />
     {props.children}
+    <Footer />
   </div>
 )
 
